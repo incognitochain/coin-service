565d199efa5767e49071d75b0c51ca3925ed1fa0
0xBahamoot <lam@incognito.org>
2021-04-05 17:31:10 +0700

    update

---
+++ privacyv2/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# cd home
+# echo "Asdasdsad"
+# BUKCETID=0
+# NUMBEROFBUCKET=5
+# MODE="indexer"
+cp /home/keylist.json ./keylist.json
+cp /home/keylist-v2.json ./keylist-v2.json
+MONGOHOST="$(getent hosts mongov2 | awk '{ print $1 }')"
+JSON='{"apiport":9001,"chaindata":"chain","fullnode":"http://139.162.55.124:8334","concurrentotacheck":10,"mode":"'"$MODE"'","mongo":"mongodb://root:example@'"$MONGOHOST"':27017","mongodb":"coin","chainnetwork":"testnet","highway":"139.162.55.124:9330","bucketid": '"$BUKCETID"',"numberofbucket":'"$NUMBEROFBUCKET"'}'
+echo $JSON 
+echo $JSON > cfg.json
+/home/coinservice 
