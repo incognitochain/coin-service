c62d7c9f782196e66f8edf55455392a33e5f934e
0xBahamoot <lam@incognito.org>
2021-09-13 17:02:01 +0700

    .

--- txservice/build.sh
+++ txservice/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-# go get -v github.com/incognitochain/incognito-chain@9a565726ab46729ce66553d619011acf8aa10ff9
 if [ "$1" == "prod" ]; then
   echo "building production tx-worker..."
   GIN_MODE=release go build -tags=jsoniter -v -o txservice
