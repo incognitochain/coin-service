995997ca3a7aa0dbf1eff289237afae0678a5632
0xBahamoot <lam@incognito.org>
2021-09-24 16:55:03 +0700

    .

---
+++ apiservice/assistant.go
@@ -0,0 +1,17 @@
+package apiservice
+
+func APIGetTop10(topType int) {
+
+}
+
+func APIGetDefaultPool() []string {
+	var result []string
+
+	return result
+}
+
+func APIGetSuggestAMP(token1, token2 string, amount1, amount2 uint64) (uint64, error) {
+	var result uint64
+
+	return result, nil
+}
