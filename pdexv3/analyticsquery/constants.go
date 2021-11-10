379fc232c0f82357f4ebe2fed98e889363d725f7
Yen Le <levu.daiyen@gmail.com>
2021-11-10 22:47:07 +0700

    remove unused analytics api

--- pdexv3/analyticsquery/constants.go
+++ pdexv3/analyticsquery/constants.go
@@ -3,7 +3,6 @@ package analyticsquery
 var AnalyticsAPIPath = map[string]string{
 	"PDEX_V3_POOL_LIQUIDITY_HISTORIES": "/api/v1/metrics/pdexv3/pool-liquidity-histories",
 	"PDEX_V3_PAIR_RATE_HISTORIES": "/api/v1/metrics/pdexv3/pair-rate-histories",
-	"PDEX_V3_PAIR_RATE_CHANGES_24H": "/api/v1/metrics/pdexv3/pair-rate-changes-24h",
 	"PDEX_V3_TRADING_VOLUME_24H": "/api/v1/metrics/pdexv3/trading-volume-24h",
 	"PDEX_V3_TRADING_VOLUME_AND_PAIR_RATE_CHANGES_24H": "/api/v1/metrics/pdexv3/pools-trading-volume-and-pair-rate-24h",
 }
