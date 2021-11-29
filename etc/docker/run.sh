2ca49316e2f831965048d80c21bb57449df51929
Yen Le <levu.daiyen@gmail.com>
2021-11-30 06:58:49 +0700

    add Dockerfile.production and entrypoint for production

---
+++ etc/docker/run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+CONFIG_JSON=$(cat <<EOF
+{
+   "apiport": $API_PORT,
+   "chaindata": "$CHAIN_DATA",
+   "concurrentotacheck": $CONCURRENT_OTA_CHECK,
+   "mode": "$MODE",
+   "mongo": "$MONGO",
+   "mongodb": "$MONGO_DB",
+   "chainnetwork": "$CHAIN_NETWORK",
+   "indexerid": $INDEXER_ID,
+   "masterindexer": "$MASTER_INDEXER",
+   "fullnodeendpoint": "$FULLNODE_ENDPOINT",
+   "analyticsAPIEndpoint": "$ANALYTICS_API_ENDPOINT",
+   "externaldecimals": "$EXTERNAL_DECIMALS"
+}
+EOF
+)
+
+echo $CONFIG_JSON
+printf "$CONFIG_JSON" > cfg.json
+
+# "$@" to pass through all arguments to script
+./coinservice "$@"
