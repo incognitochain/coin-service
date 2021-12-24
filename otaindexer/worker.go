aa84f10ebf63ee05fe797231a1ce1e91d199863a
0xBahamoot <lam@incognito.org>
2021-12-24 11:59:03 +0700

    update

--- otaindexer/worker.go
+++ otaindexer/worker.go
@@ -23,6 +23,7 @@ import (
 	"github.com/kamva/mgm/v3/operator"
 	uuid "github.com/satori/go.uuid"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo/options"
 
 	metadataCommon "github.com/incognitochain/incognito-chain/metadata/common"
@@ -613,20 +614,35 @@ func getTxToProcess(lastID map[int]string, limit int64) ([]shared.TxData, error)
 	for shardID, v := range lastID {
 		var txList []shared.TxData
 		metas := []string{strconv.Itoa(metadataCommon.Pdexv3TradeRequestMeta)}
-		filter := bson.M{
-			"_id":             bson.M{operator.Gt: v},
-			"shardid":         bson.M{operator.Eq: shardID},
-			"metatype":        bson.M{operator.In: metas},
-			"pubkeyreceivers": bson.M{operator.Size: 0},
+		var obID primitive.ObjectID
+		var err error
+		if v == "" {
+			obID = primitive.ObjectID{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+		} else {
+			obID, err = primitive.ObjectIDFromHex(v)
+			if err != nil {
+				return nil, err
+			}
 		}
-		err := mgm.Coll(&shared.TxData{}).SimpleFind(&txList, filter, &options.FindOptions{
+
+		filter := bson.M{
+			"_id":      bson.M{operator.Gt: obID},
+			"shardid":  bson.M{operator.Eq: shardID},
+			"metatype": bson.M{operator.In: metas},
+		}
+		err = mgm.Coll(&shared.TxData{}).SimpleFind(&txList, filter, &options.FindOptions{
 			Sort:  bson.D{{"locktime", 1}},
 			Limit: &limit,
 		})
 		if err != nil {
 			return nil, err
 		}
-		result = append(result, txList...)
+		for _, v := range txList {
+			if len(v.PubKeyReceivers) == 0 {
+				result = append(result, v)
+			}
+		}
+
 	}
 
 	return result, nil
@@ -640,6 +656,11 @@ func GetOTAKeyListMinScannedTxIndex() map[int]string {
 		for _, keyData := range keys {
 			if strings.Compare(keyData.KeyInfo.LastScanTxID, minTxIdx[shardID]) == -1 {
 				minTxIdx[shardID] = keyData.KeyInfo.LastScanTxID
+			}
+			if keyData.KeyInfo.LastScanTxID == "" {
+				id := primitive.ObjectID{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+				minTxIdx[shardID] = id.Hex()
+				break
 			}
 		}
 	}
