fb61f2d3a89744542a2e9cf60c474ccc92f00479
0xBahamoot <lam@incognito.org>
2022-02-10 15:25:29 +0700

    update

--- apiservice/pdexv3.go
+++ apiservice/pdexv3.go
@@ -1861,3 +1861,241 @@ func (pdexv3) PendingLimit(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, respond)
 }
+
+func (pdexv3) ListMarkets(c *gin.Context) {
+	var datalist []TokenInfo
+
+	defaultPools, err := database.DBGetDefaultPool(true)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, buildGinErrorRespond(err))
+		return
+	}
+	priorityTokens, err := database.DBGetTokenPriority()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, buildGinErrorRespond(err))
+		return
+	}
+	tokenMap := make(map[string]struct{})
+	tokens := []string{}
+	for poolID := range defaultPools {
+		tks := strings.Split(poolID, "-")
+		tokenMap[tks[0]] = struct{}{}
+		tokenMap[tks[1]] = struct{}{}
+	}
+	for v := range tokenMap {
+		tokens = append(tokens, v)
+	}
+	extraTokenInfo, err := database.DBGetExtraTokenInfoByTokenID(tokens)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, buildGinErrorRespond(err))
+		return
+	}
+	customTokenInfo, err := database.DBGetCustomTokenInfoByTokenID(tokens)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, buildGinErrorRespond(err))
+		return
+	}
+
+	extraTokenInfoMap := make(map[string]shared.ExtraTokenInfo)
+	for _, v := range extraTokenInfo {
+		extraTokenInfoMap[v.TokenID] = v
+	}
+
+	customTokenInfoMap := make(map[string]shared.CustomTokenInfo)
+	for _, v := range customTokenInfo {
+		customTokenInfoMap[v.TokenID] = v
+	}
+	chainTkListMap := make(map[string]struct{})
+
+	baseToken, _ := database.DBGetBasePriceToken()
+
+	prvUsdtPair24h := float64(0)
+	for v, _ := range defaultPools {
+		if strings.Contains(v, baseToken) && strings.Contains(v, common.PRVCoinID.String()) {
+			prvUsdtPair24h = getPoolPair24hChange(v)
+			break
+		}
+	}
+
+	tokenList, err := database.DBGetTokenByTokenID(tokens)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, buildGinErrorRespond(err))
+		return
+	}
+
+	for _, v := range tokenList {
+		chainTkListMap[v.TokenID] = struct{}{}
+		currPrice, _ := strconv.ParseFloat(v.CurrentPrice, 64)
+		pastPrice, _ := strconv.ParseFloat(v.PastPrice, 64)
+		percent24h := float64(0)
+		if pastPrice != 0 && currPrice != 0 {
+			percent24h = ((currPrice - pastPrice) / pastPrice) * 100
+		}
+		data := TokenInfo{
+			TokenID:          v.TokenID,
+			Name:             v.Name,
+			Symbol:           v.Symbol,
+			Image:            v.Image,
+			IsPrivacy:        v.IsPrivacy,
+			IsBridge:         v.IsBridge,
+			ExternalID:       v.ExternalID,
+			PriceUsd:         currPrice,
+			PercentChange24h: fmt.Sprintf("%.2f", percent24h),
+		}
+
+		defaultPool := ""
+		defaultPairToken := ""
+		defaultPairTokenIdx := -1
+		currentPoolAmount := uint64(0)
+		for poolID, _ := range defaultPools {
+			if strings.Contains(poolID, data.TokenID) {
+				pa := getPoolAmount(poolID, data.TokenID)
+				if pa == 0 {
+					continue
+				}
+				tks := strings.Split(poolID, "-")
+				tkPair := tks[0]
+				if tks[0] == data.TokenID {
+					tkPair = tks[1]
+				}
+				for idx, ptk := range priorityTokens {
+					if (ptk == tkPair) && (idx >= defaultPairTokenIdx) {
+						if idx > defaultPairTokenIdx {
+							defaultPool = poolID
+							defaultPairToken = tkPair
+							defaultPairTokenIdx = idx
+							currentPoolAmount = pa
+						}
+						if (idx == defaultPairTokenIdx) && (pa > currentPoolAmount) {
+							defaultPool = poolID
+							defaultPairToken = tkPair
+							defaultPairTokenIdx = idx
+							currentPoolAmount = pa
+						}
+					}
+				}
+
+				if defaultPool == "" {
+					if pa > 0 {
+						defaultPool = poolID
+						defaultPairToken = tkPair
+						currentPoolAmount = pa
+					}
+				} else {
+					if (pa > currentPoolAmount) && (defaultPairTokenIdx == -1) {
+						defaultPool = poolID
+						defaultPairToken = tkPair
+						currentPoolAmount = pa
+					}
+				}
+			}
+		}
+		data.DefaultPairToken = defaultPairToken
+		data.DefaultPoolPair = defaultPool
+		if data.TokenID == common.PRVCoinID.String() {
+			data.PercentChange24h = fmt.Sprintf("%.2f", prvUsdtPair24h)
+		} else {
+			if data.DefaultPairToken != "" && data.TokenID != baseToken {
+				data.PercentChange24h = fmt.Sprintf("%.2f", getToken24hPriceChange(data.TokenID, data.DefaultPairToken, data.DefaultPoolPair, baseToken, prvUsdtPair24h))
+			}
+		}
+
+		if etki, ok := customTokenInfoMap[v.TokenID]; ok {
+			if etki.Name != "" {
+				data.Name = etki.Name
+			}
+			if etki.Symbol != "" {
+				data.Symbol = etki.Symbol
+			}
+			if etki.Verified {
+				data.Verified = etki.Verified
+			}
+		}
+		if etki, ok := extraTokenInfoMap[v.TokenID]; ok {
+			if etki.Name != "" {
+				data.Name = etki.Name
+			}
+			data.Decimals = etki.Decimals
+			if etki.Symbol != "" {
+				data.Symbol = etki.Symbol
+			}
+			data.PSymbol = etki.PSymbol
+			data.PDecimals = int(etki.PDecimals)
+			data.ContractID = etki.ContractID
+			data.Status = etki.Status
+			data.Type = etki.Type
+			data.CurrencyType = etki.CurrencyType
+			data.Default = etki.Default
+			if etki.Verified {
+				data.Verified = etki.Verified
+			}
+			data.UserID = etki.UserID
+			data.PercentChange1h = etki.PercentChange1h
+			data.PercentChangePrv1h = etki.PercentChangePrv1h
+			data.CurrentPrvPool = etki.CurrentPrvPool
+			data.PricePrv = etki.PricePrv
+			data.Volume24 = etki.Volume24
+			data.ParentID = etki.ParentID
+			data.OriginalSymbol = etki.OriginalSymbol
+			data.LiquidityReward = etki.LiquidityReward
+			data.Network = etki.Network
+			err = json.UnmarshalFromString(etki.ListChildToken, &data.ListChildToken)
+			if err != nil {
+				panic(err)
+			}
+			if data.PriceUsd == 0 {
+				data.PriceUsd = etki.PriceUsd
+			}
+
+		}
+
+		if !v.IsNFT {
+			datalist = append(datalist, data)
+		}
+	}
+
+	for _, tkInfo := range extraTokenInfo {
+		if _, ok := chainTkListMap[tkInfo.TokenID]; !ok {
+			tkdata := TokenInfo{
+				TokenID:      tkInfo.TokenID,
+				Name:         tkInfo.Name,
+				Symbol:       tkInfo.Symbol,
+				PSymbol:      tkInfo.PSymbol,
+				PDecimals:    int(tkInfo.PDecimals),
+				Decimals:     tkInfo.Decimals,
+				ContractID:   tkInfo.ContractID,
+				Status:       tkInfo.Status,
+				Type:         tkInfo.Type,
+				CurrencyType: tkInfo.CurrencyType,
+				Default:      tkInfo.Default,
+				Verified:     tkInfo.Verified,
+				UserID:       tkInfo.UserID,
+
+				PriceUsd:           tkInfo.PriceUsd,
+				PercentChange1h:    tkInfo.PercentChange1h,
+				PercentChangePrv1h: tkInfo.PercentChangePrv1h,
+				CurrentPrvPool:     tkInfo.CurrentPrvPool,
+				PricePrv:           tkInfo.PricePrv,
+				Volume24:           tkInfo.Volume24,
+				ParentID:           tkInfo.ParentID,
+				OriginalSymbol:     tkInfo.OriginalSymbol,
+				LiquidityReward:    tkInfo.LiquidityReward,
+
+				Network: tkInfo.Network,
+			}
+			err = json.UnmarshalFromString(tkInfo.ListChildToken, &tkdata.ListChildToken)
+			if err != nil {
+				panic(err)
+			}
+			datalist = append(datalist, tkdata)
+		}
+	}
+	respond := APIRespond{
+		Result: datalist,
+		Error:  nil,
+	}
+	c.JSON(http.StatusOK, respond)
+}
