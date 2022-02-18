e85d519918d71debff951073294f6c4c6b455625
0xBahamoot <lam@incognito.org>
2022-02-18 16:24:34 +0700

    update api

--- apiservice/coin.go
+++ apiservice/coin.go
@@ -315,10 +315,10 @@ func APIGetTokenInfo(c *gin.Context) {
 			percent24h = ((currPrice - pastPrice) / pastPrice) * 100
 		}
 		data := TokenInfo{
-			TokenID:          v.TokenID,
-			Name:             v.Name,
-			Symbol:           v.Symbol,
-			Image:            v.Image,
+			TokenID: v.TokenID,
+			Name:    v.Name,
+			Symbol:  v.Symbol,
+			// Image:            v.Image,
 			IsPrivacy:        v.IsPrivacy,
 			IsBridge:         v.IsBridge,
 			ExternalID:       v.ExternalID,
@@ -389,6 +389,9 @@ func APIGetTokenInfo(c *gin.Context) {
 			}
 			if etki.Symbol != "" {
 				data.Symbol = etki.Symbol
+			}
+			if etki.Image != "" {
+				data.Image = etki.Image
 			}
 			if etki.Verified {
 				data.Verified = etki.Verified
